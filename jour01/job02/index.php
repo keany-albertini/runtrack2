3fa62ff9664709ac3d438eabace16fdd5197f3b9
albertini <keany.albertini@laplateforme.io>
2020-11-02 13:03:18 +0100

    ajout du job 02

---
+++ jour01/job02/index.php
@@ -0,0 +1,16 @@
+<?php
+$str="Laplateforme ";
+
+$str2="vive ";
+
+
+$str3="!";
+
+
+// premier texte + intégration du retour a la ligne 
+echo $str2 . $str . $str3 . "<br />\n";
+
+/*la deuxieme ligne de text*/
+echo $str2 . $str . $str3;
+?>
+
