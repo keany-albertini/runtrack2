09dfea374f6b22d6451cacb369187548c409c232
albertini <keany.albertini@laplateforme.io>
2020-11-02 16:04:45 +0100

    envoi du tableau finition du jour 1

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -2,75 +2,74 @@
 <html>
 <head>
 	<style type="text/css">
+		body{
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+		}
+		h1{
+			text-align: center;
+		}
 		table{
+			border:2px solid black;
+			width: 300px;
 			margin-top: 20px;
 			display: flex;
 			justify-content: center;
+			box-shadow: 6px 3px 3px lightgrey;
 		}
 		td{
-			border:1px solid black;
-			
+			border-bottom:1px solid black;
 			padding: 15px;
 		}
 		th{
+			box-shadow: 6px 3px 3px lightgrey;
 			color: blue;
 			border:1px solid black;
 		}
-		
+		th:hover{
+			background:blue;
+			color: white;
+		}
 	</style>
-	<title></title>
+	<title>Tableau Variables</title>
 </head>
-<body>
-<?php
-$tableau = ["type","nom","valeur"];
-$nombre=10;// type entiers
-$booleen= true;// type booléens
-$nom="marseille"; // type chaine de caractère
-$virgule=1.5;// nombre a virgule flotante
-
-
-echo "<table>
-	<tr>
-		<th> $tableau[0];</th>
-		<th> $tableau[1];</th>
-		<th> $tableau[2];</th>
-
-
-
-
-
-	</tr>
-	<tr>
-		
-		<td>  .'gettype($nombre)'.; </td> 
-		<td> '$nombre';</td>
-		<td> $nombre; </td> 
-	</tr>
-	<tr>
-		
-		<td>print "gettype($booleen)"; </td>
-		<td>'$booleen';</td>
-		<td>$booleen; </td>
-	</tr>
-	<tr>
-		
-		<td> gettype($nom); </td>
-		<td>'$nom'</td>
-		<td>$nom; </td>
-	</tr>
-	<tr>
-		
-		<td>  gettype($virgule);</td>
-		<td> '$virgule'</td>
-		<td> $virgule; </td>
-	</tr>
-	
-
-
-
-</table>";
-
-
-
-
+	<body>
+		<h1>Tableau variables</h1>
+		<?php
+			$tableau = ["type","nom","valeur"];
+			$nombre=10;// type entiers
+			$booleen= true;// type booléens
+			$nom="marseille"; // type chaine de caractère
+			$virgule=1.5;// nombre a virgule flotante
+		?>
+		<table>
+			<tr>
+				<th><?php echo $tableau[0];?></th>
+				<th><?php echo $tableau[1];?></th>
+				<th><?php echo $tableau[2];?></th>
+			</tr>
+			<tr>
+				<td><?php echo  gettype($nombre); ?></td> <!-- afficher le type -->
+				<td><?php echo '$nombre';?></td> <!-- afficher le nom -->
+				<td><?php echo $nombre; ?></td> <!-- afficher la valeur -->
+			</tr>
+			<tr>
+				<td><?php echo  gettype($booleen); ?></td>
+				<td><?php echo '$booleen';?></td>
+				<td><?php echo $booleen; ?></td>
+			</tr>
+			<tr>
+				<td><?php echo  gettype($nom); ?></td>
+				<td><?php echo '$nom';?></td>
+				<td><?php echo $nom; ?></td>
+			</tr>
+			<tr>
+				<td><?php echo  gettype($virgule); ?></td>
+				<td><?php echo '$virgule';?></td>
+				<td><?php echo $virgule; ?></td>
+			</tr>
+		</table>
+	</body>
+</html>
 
